83692ef07e49c15455cb94c3ef258be48c991f51
richard-allcca <richard_allcca_llano@hotmail.com>
2023-12-08 18:59:37 -0500

    add home screen and menu items

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({
+    required this.title,
+    required this.subTitle,
+    required this.link,
+    required this.icon
+  });
+}
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+    title: 'Botones',
+    subTitle: 'Varios botones',
+    link: '/button',
+    icon: Icons.smart_button_outlined
+  )
+];
