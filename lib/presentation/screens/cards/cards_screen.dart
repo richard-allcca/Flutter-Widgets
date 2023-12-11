af34625afc747749de68e8405a9176e4e2d8e439
richard-allcca <richard_allcca_llano@hotmail.com>
2023-12-11 17:04:30 -0500

    add routes with name of go_route

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class CardsScreen extends StatelessWidget {
+
+  static const String name = 'cards_Screen';
+
   const CardsScreen({super.key});
 
   @override
