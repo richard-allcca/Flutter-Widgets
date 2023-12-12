df7f67924c4ea586535334c363acd9e7983fc623
richard-allcca <richard_allcca_llano@hotmail.com>
2023-12-11 22:03:15 -0500

    added screens and update barrel

---
+++ lib/presentation/screens/infinity_scroll/infinity_scroll_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class InfinityScrollScreen extends StatelessWidget {
+
+  static const name = 'infinity_screen';
+
+  const InfinityScrollScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
