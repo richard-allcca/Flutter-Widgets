56a736341125f402a290d50cc24b3d5157ce87ec
Thouma <richard_allcca_llano@hotmail.com>
2024-03-16 06:31:15 -0500

    end section added custom widget with state

--- lib/presentation/screens/counter/counter_function_screen.dart
+++ lib/presentation/screens/counter/counter_function_screen.dart
@@ -23,72 +23,93 @@ class _CounterFunctionScreenState extends State<CounterFunctionScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Counter Functions'),
-        centerTitle: true,
-        // NOTE - ubica por default un icono a la izquierda
-        leading: IconButton(
-          icon: const Icon(Icons.refresh_outlined),
-          onPressed: () {
-            setState(() {
-              clickCounter = 0;
-            });
-          },
-        ),
-        // NOTE - ubica por un icono a la Derecha pero puede recibir mas de 1
-        actions: [
-          IconButton(
+        appBar: AppBar(
+          title: const Text('Counter Functions'),
+          centerTitle: true,
+          // NOTE - ubica por default un icono a la izquierda
+          leading: IconButton(
             icon: const Icon(Icons.refresh_outlined),
             onPressed: () {
               setState(() {
                 clickCounter = 0;
               });
             },
-          )
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('$clickCounter',
-                style: const TextStyle(
-                    fontSize: 160, fontWeight: FontWeight.w100)),
-            Text('Click${clickCounter == 1 ? '' : 's'}',
-                style: const TextStyle(fontSize: 25))
+          ),
+          // NOTE - ubica por un icono a la Derecha pero puede recibir mas de 1
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.refresh_outlined),
+              onPressed: () {
+                setState(() {
+                  clickCounter = 0;
+                });
+              },
+            )
           ],
         ),
-      ),
-      floatingActionButton: Column(
+
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('$clickCounter',
+                  style: const TextStyle(
+                      fontSize: 160, fontWeight: FontWeight.w100)),
+              Text('Click${clickCounter == 1 ? '' : 's'}',
+                  style: const TextStyle(fontSize: 25))
+            ],
+          ),
+        ),
+        floatingActionButton: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            FloatingActionButton(
-              onPressed: () {
-                // 1° forma de cambiar el estado
-                setState(() {
+            CustomFloatBtn(
+                icon: Icons.refresh_outlined,
+                onPressed: () {
+                  // 1° forma de cambiar el estado
+                  setState(() {
+                    clickCounter = 0;
+                  });
+                }),
+            const SizedBox(height: 10),
+            CustomFloatBtn(
+                icon: Icons.plus_one,
+                onPressed: () {
+                  // 2° forma de cambiar el estado
                   clickCounter++;
-                });
-                // 2° forma de cambiar el estado
-                //   clickCounter++;
-                // setState(() {});
-              },
-              child: const Icon(Icons.plus_one),
+                  setState(() {});
+                },
             ),
-            const SizedBox(height: 10,),
-            FloatingActionButton(
-              onPressed: () {
-                // 1° forma de cambiar el estado
-                setState(() {
+            const SizedBox(height: 10),
+            CustomFloatBtn(
+                icon: Icons.exposure_minus_1,
+                onPressed: () {
+                  if(clickCounter == 0) return;
                   clickCounter--;
-                });
-                // 2° forma de cambiar el estado
-                //   clickCounter++;
-                // setState(() {});
-              },
-              child: const Icon(Icons.exposure_minus_1),
+                  setState(() {});
+                },
             ),
           ],
-        )
+        ));
+  }
+}
+
+class CustomFloatBtn extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback? onPressed;
+
+  const CustomFloatBtn({
+    super.key,
+    required this.icon,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      shape: const StadiumBorder(),
+      onPressed: onPressed,
+      child: Icon(icon),
     );
   }
 }
